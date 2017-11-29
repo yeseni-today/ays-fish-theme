17a655a615de999482c7bcd772e624e2fbfee8fc
aollio <aollio@outlook.com>
2017-11-29 09:08:30 +0800

    nothing

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,20 +1,22 @@
 function fish_prompt
   # Cache exit status
   set -l last_status $status
-  set -l __work_dir (string replace $HOME '~' (pwd))
+
 
   # Just calculate these once, to save a few cycles when displaying the prompt
   if not set -q __fish_prompt_hostname
     set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
   end
-  if not set -q __fish_prompt_char
+  if not set -q __fish_user
     switch (id -u)
       case 0
-	set -g __fish_prompt_char '#'
+	     set -g __fish_user '#'
       case '*'
-	set -g __fish_prompt_char '~'
+	     set -g __fish_user '&'
     end
   end
+
+  set -g __fish_prompt_char '~'
 
   # Setup colors
   set -l normal (set_color normal)
@@ -39,23 +41,26 @@ function fish_prompt
   set -g __fish_git_prompt_showstashstate true
   set -g __fish_git_prompt_show_informative_status true 
  
+  set -l __work_dir (string replace $HOME '~' (pwd))
 
   # Line 1
-  echo -n $green'╭ '$blue$USER$white' @ '$green$__fish_prompt_hostname$white' in '$yellow$__work_dir$turquoise
-  __fish_git_prompt " (%s)"
-  echo
+  echo -n $purple$__fish_user' '$blue$USER$white' @ '$green$__fish_prompt_hostname$white' in '$yellow$__work_dir$turquoise
+  __fish_git_prompt;
+
+  #╭
+  #╰
 
   # Line 2
-  echo -n $green'╰'
-  # support for virtual env name
-  if set -q VIRTUAL_ENV
-      echo -n "($green"(basename "$VIRTUAL_ENV")"$green)"
-  end
+  echo 
+  #support for virtual env name
+  # if set -q VIRTUAL_ENV
+  #    echo -n "($green"(basename "$VIRTUAL_ENV")"$green)"
+  #end
   
   if test $last_status -eq 0
-    echo -n $green' '$__fish_prompt_char $green
+   echo -n $green$__fish_prompt_char$green' ' 
   else
-    echo -n $red' 'x $red
+   echo -n $red'x '$red
   end
 end
 
